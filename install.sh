a28a119b778abc61625648cb15ee91f3904adaf1
Steven Harradine <stevenharradine@gmail.com>
2017-08-26 13:05:19 -0400

    user generic installer

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 installPath=/usr/local/bin
 program=qBash
 
-if [ "$EUID" -ne 0 ]; then
-	echo "Please run as root"
-else
-	echo -n "Installing . "
-	cp $program.sh $installPath/$program
-	chown root:root $installPath/$program
-	chmod 755 $installPath/$program
-	echo "Done"
-fi
+wget -O installer.sh https://raw.githubusercontent.com/stevenharradine/bashInstaller/master/installer.sh
+source installer.sh
+rm installer.sh

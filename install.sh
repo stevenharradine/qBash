a0ecd65aef507a37f7164a8d897898368b04588c
Steven Harradine <stevenharradine@gmail.com>
2017-08-27 00:55:58 -0400

    installer uses encapsulated format

--- install.sh
+++ install.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-installPath=/usr/local/bin
-program=qBash
-
-wget -O installer.sh https://raw.githubusercontent.com/stevenharradine/bashInstaller/master/installer.sh
-source installer.sh
-rm installer.sh
+curl https://raw.githubusercontent.com/stevenharradine/bashInstaller/master/installer.sh | bash -s program=qBash

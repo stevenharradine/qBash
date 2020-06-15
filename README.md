4ccfc484d9fafcf9d800864e99ba5c637d923973
Steven Harradine <stevenharradine@gmail.com>
2020-06-15 18:43:13 -0400

    add proper argument handling, add delay option

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ QueueBash executes bash commands passing in a variable from a queue.
 ## Usage
 **Note:** the input file is distructive, it is the queue and as items are popped off they are lost
 ```
-./qBash.sh {{ command }} {{ inputfile }}
+./qBash.sh --command=echo --inputFile=queue.list --delay=0
 ```
 ### Where,
  * `command` is the bash command to be executed
- * `inputfile` is a line delimited file with the parameters to be appended to the end of the command
+ * `inputFile` is a line delimited file with the parameters to be appended to the end of the command
+ * `delay` (optional) is the amount of time to sleep between runs
+ * `help` displays help screen
 
 ## Example (installed)
 1. Install
@@ -25,10 +27,10 @@ echo "World!" >> in
 ```
 3. Execute your command across all lines in the input file
 ```
-qBash echo in
+qBash --command=echo --inputFile=in
 ```
 ```
-~/qBash$ qBash echo in
+~/qBash$ qBash --command=echo --inputFile=in
 Hello
 World!
 ```
@@ -42,10 +44,10 @@ echo "World!" >> in
 ```
 2. Execute your command across all lines in the input file
 ```
-./qBash.sh echo in
+./qBash.sh --command=echo --inputFile=in
 ```
 ```
-~/qBash$ ./qBash.sh echo in
+~/qBash$ ./qBash.sh --command=echo --inputFile=in
 Hello
 World!
 ```
--- qBash.sh
+++ qBash.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
-# (c) 2017 Steven Harradine
-command=$1
-input=$2
+# (c) 2020 Steven Harradine
+version=2.0.0
+delay=0
+command=""
+input=""
+help=""
 
-currentArgument=`head -n 1 "$input"`                                           # get the first element from the list
-echo $($command "$currentArgument")                                            # execute command with argument
-echo "$(tail -n +2 $input)" > $input                                           # delete top element from the list
+for arg in "$@"; do
+		key=`echo "$arg" | awk -F "=" '{print $1}'`
+		value=`echo "$arg" | awk -F "=" '{print $2}'`
 
-if [[ ! ( "`head -n 1 $input`" == "" && "`cat $input | wc -l`" -eq 1 ) ]];then # if the list is not empty
-	if [[ "`which qBash | wc -l`" -eq 1 ]];then                                # is this command installed?
-		cmd="qBash"
-	else                                                                       # or run from script
-		cmd="./qBash.sh"
+		if [[ $key == "--command" ]]; then
+				command=$value
+		elif [[ $key == "--inputFile" ]]; then
+				input=$value
+		elif [[ $key == "--delay" ]]; then
+				delay=$value
+		elif [[ $key == "--help" ]]; then
+				help="me"
+		fi
+done
+
+if [[ $help != "" ]]; then
+	echo "qBash Version $version"
+	echo "executes bash commands passing in a variable from a queue."
+	echo ""
+	echo "Usage:"
+	echo "**Note:** the input file is distructive, it is the queue and as items are popped off they are lost"
+	echo "	./qBash.sh --command=echo --inputFile=queue.list --delay=0"
+	echo "Where,"
+	echo " * 'command' is the bash command to be executed"
+	echo " * 'inputFile' is a line delimited file with the parameters to be appended to the end of the command"
+	echo " * 'delay' (optional) is the amount of time to sleep between runs"
+	echo " * 'help' displays this help screen"
+	echo ""
+elif [[ $command == "" ]]; then
+	echo "You must define 'command' argument"
+elif [[ $input == "" ]]; then
+	echo "You must define 'inputFile' argument"
+else
+	currentArgument=`head -n 1 "$input"`                                           # get the first element from the list
+	echo $($command "$currentArgument")                                            # execute command with argument
+	echo "$(tail -n +2 $input)" > $input                                           # delete top element from the list
+
+	if [[ ! ( "`head -n 1 $input`" == "" && "`cat $input | wc -l`" -eq 1 ) ]];then # if the list is not empty
+		if [[ "`which qBash | wc -l`" -eq 1 ]];then                                # is this command installed?
+			cmd="qBash"
+		else                                                                       # or run from script
+			cmd="./qBash.sh"
+		fi
+		
+		sleep $delay
+		$cmd --command="$command" --inputFile=$input --delay=$delay                # recursively execute this function with the same parameters
 	fi
-	
-	$cmd "$1" $2                                                               # recursively execute this function with the same parameters
 fi
